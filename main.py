8fb90d829b6121af398467890e756dd493679afc
paichiwo <lzerucha@gmail.com>
2023-09-20 17:32:46 +0100

    Implement Player class skeleton

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ import pygame
 class Road:
     """Endless scrolling, TOPDOWN [ top to bottom ]"""
     def __init__(self):
-
         self.image = pygame.image.load("img/road.png").convert_alpha()
         self.image_height = self.image.get_height()
-
         self.scroll = 0
         self.panels = math.ceil(WINDOW_HEIGHT / self.image_height + 2)
 
     def update(self):
         self.scroll += 3
         for i in range(self.panels):
-            y_pos = int(i * self.image_height + self.scroll - self.image_height)
+            y_pos = int((i * self.image_height) + self.scroll - self.image_height)
             screen.blit(self.image, (0, y_pos))
             if abs(self.scroll) >= self.image_height:
                 self.scroll = 0
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("img/car.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, 2)
+        self.rect = self.image.get_rect(midbottom=(300, 700))
+
+    def movement(self):
+        pass
+
+    def update(self):
+        pass
 
 
 def fps_counter():
@@ -41,16 +53,22 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SCALED, v
 
 road = Road()
 
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     road.update()
+
+    player.draw(screen)
+    player.update()
+
     fps_counter()
 
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
-
