1edd341b99d04469e9882a19d4eb866664718f6d
paichiwo <lzerucha@gmail.com>
2023-10-02 11:46:02 +0100

    Implement timer for the game in the show_time func

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 class Road:
     """Endless scrolling, TOPDOWN [ top to bottom ]"""
     def __init__(self):
-        self.elapsed_time = None
         self.image = pygame.image.load("img/road.png").convert_alpha()
         self.image_height = self.image.get_height()
 
@@ -115,6 +114,17 @@ def fps_counter():
     screen.blit(fps_text, (460, 750))
 
 
+def show_time(start):
+    current_time = pygame.time.get_ticks()
+    elapsed_time = (current_time-start) // 1000
+    minutes = elapsed_time // 60
+    seconds = elapsed_time % 60
+    timer_text = "{:02}:{:02}".format(minutes, seconds)
+
+    time_text = FONT.render(timer_text, 1, "Black")
+    screen.blit(time_text, (460, 700))
+
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -124,12 +134,13 @@ REFERENCE_POINT = 0
 FONT = pygame.font.Font('font/joystix_mono.otf', 18)
 running = True
 
+start_time = pygame.time.get_ticks()
+
 pygame.display.set_caption("Endless Scrolling")
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SCALED, vsync=1)
 
 road = Road()
-player = pygame.sprite.GroupSingle()
-player.add(Player())
+player = pygame.sprite.GroupSingle(Player())
 
 # MAIN GAME LOOP
 while running:
@@ -142,6 +153,7 @@ while running:
     player.update()
 
     fps_counter()
+    show_time(start_time)
 
     pygame.display.update()
     clock.tick(60)
