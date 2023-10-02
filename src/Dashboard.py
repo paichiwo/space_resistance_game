67b2ed1e400f4d9e3a5f80c771e35f33464e7b58
paichiwo <lzerucha@gmail.com>
2023-10-02 16:29:08 +0100

    Implement draw method, create color variable for font

--- src/Dashboard.py
+++ src/Dashboard.py
@@ -4,23 +4,27 @@ import pygame
 class DashBoard:
     """Creates a dashboard object containing game statistics"""
 
-    def __init__(self, screen, clock, start_time, font):
+    def __init__(self, screen, clock, start_time):
         super().__init__()
 
         self.screen = screen
         self.clock = clock
         self.start_time = start_time
-        self.speed = 0
-        self.font = font
+        self.font = pygame.font.Font("font/PIXEARG_.TTF", 13)
+        self.font_color = "White"
+
+    def draw(self):
+        """Draw and display the dashboard background"""
+        pygame.draw.rect(self.screen, "black", pygame.Rect(0, 730, 600, 70))
 
     def show_speed(self, speed):
         """Display speed information"""
-        speed_text = self.font.render(f"{speed} km/h", 1, "Black")
+        speed_text = self.font.render(f"{speed} km/h", 0, self.font_color)
         self.screen.blit(speed_text, (10, 750))
 
     def show_distance(self, distance):
         """Display distance information"""
-        distance_text = self.font.render("{:.2f} km".format(distance), 1, "Black")
+        distance_text = self.font.render("{:.2f} km".format(distance), 0, self.font_color)
         self.screen.blit(distance_text, (10, 700))
 
     def show_time(self):
@@ -30,16 +34,17 @@ class DashBoard:
         minutes = elapsed_time // 60
         seconds = elapsed_time % 60
         timer_text = "{:02}:{:02}".format(minutes, seconds)
-        time_text = self.font.render(timer_text, 1, "Black")
+        time_text = self.font.render(timer_text, 0, self.font_color)
         self.screen.blit(time_text, (460, 700))
 
     def show_fps_counter(self):
         """Display the current FPS rate"""
         fps = str(round(self.clock.get_fps(), 2))
-        fps_text = self.font.render(f"FPS {fps}", 1, "Black")
+        fps_text = self.font.render(f"FPS {fps}", 0, self.font_color)
         self.screen.blit(fps_text, (460, 750))
 
     def update(self, speed, distance):
+        self.draw()
         self.show_speed(speed)
         self.show_distance(distance)
         self.show_time()
