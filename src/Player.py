ea837c33a55b4f48bece397d26f6709c02411fee
paichiwo <lzerucha@gmail.com>
2023-10-02 20:19:31 +0100

    Change player image

--- src/Player.py
+++ src/Player.py
@@ -7,7 +7,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("img/car.png").convert_alpha()
+        self.image = pygame.image.load("img/player.png").convert_alpha()
         self.image = pygame.transform.rotozoom(self.image, 0, 2)
         self.rect = self.image.get_rect(midbottom=(300, 700))
 
