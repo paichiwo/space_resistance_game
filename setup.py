a58b31bdc5655858d21ed5a8c3ce801cb4254886
paichiwo <lzerucha@gmail.com>
2023-10-02 14:30:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Racing Game',
     version='0.0.1',
-    packages=['pygame'],
+    packages=find_packages(),
     url='https://github.com/paichiwo/racing_pygame',
     license='MIT',
     author='Lukasz Zerucha',
     author_email='lzerucha@gmail.com',
-    description='Retro style racing game'
+    description='Retro-style racing game',
+    install_requires=['pygame']
 )
