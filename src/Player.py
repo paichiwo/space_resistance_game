a519321fbedb01656b75b5f19860798a1327eb13
paichiwo <lzerucha@gmail.com>
2023-10-03 12:49:10 +0100

    Refactor collision with a kerb into stay_on_road method, create variables to control callback when LEFT or RIGHT is pressed

--- src/Player.py
+++ src/Player.py
@@ -11,17 +11,23 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.rotozoom(self.image, 0, 2)
         self.rect = self.image.get_rect(midbottom=(300, 700))
 
+        self.turn_speed = 3
+        self.move_left = False
+        self.move_right = False
+
     def movement(self):
         """Move the player's car left or right based on user input."""
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 3
-            if self.rect.x <= 160:
-                self.rect.x += 10
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 3
-            if self.rect.x >= 390:
-                self.rect.x -= 10
+        if self.move_left:
+            self.rect.x += self.turn_speed
+        if self.move_right:
+            self.rect.x -= self.turn_speed
+
+    def stay_on_road(self):
+        if self.rect.x <= 160:
+            self.rect.x += 10
+        if self.rect.x >= 390:
+            self.rect.x -= 10
 
     def update(self):
         self.movement()
+        self.stay_on_road()
