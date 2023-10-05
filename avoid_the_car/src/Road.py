00d1d57d15a51e5ae9b539e367b5b84aa0564fb8
paichiwo <lzerucha@gmail.com>
2023-10-05 21:20:10 +0100

    Refactor road to have only scroll attribute

--- avoid_the_car/src/Road.py
+++ avoid_the_car/src/Road.py
@@ -1,5 +1,5 @@
 import pygame
-import time
+from avoid_the_car.src.utils import get_level_info
 
 
 class Road:
@@ -14,25 +14,20 @@ class Road:
         self.image_height = self.image.get_height()
 
         self.scroll = 0
-        self.speed = 70
-        self.start_time = time.time()
 
     def scrolling(self):
         """Endless scroll method"""
         self.scroll += 4
         if self.scroll >= self.image_height:
             self.scroll = 0
-
         blit_offset = self.scroll % self.image_height
         self.screen.blit(self.image, (0, blit_offset))
         self.screen.blit(self.image, (0, blit_offset - self.image_height))
 
     def movement(self, level, levels_data):
         """Adjust scrolling speed based on user input"""
-        if levels_data and f"level{level}" in levels_data:
-            level_info = levels_data[f"level{level}"]
-            self.scroll += level_info["scroll"]
-            self.speed = level_info["speed"]
+        level_info = get_level_info(level, levels_data)
+        self.scroll += level_info["scroll"]
 
     def update(self, level, levels_data):
         self.scrolling()
