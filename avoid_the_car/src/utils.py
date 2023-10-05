f0d250a93153f326eb73123bd580cf7c36e2a332
paichiwo <lzerucha@gmail.com>
2023-10-05 21:17:55 +0100

    Implement get_level_info

--- avoid_the_car/src/utils.py
+++ avoid_the_car/src/utils.py
@@ -6,3 +6,9 @@ def load_level_data():
     with open("data/levels.json", "r") as json_file:
         level_data = json.load(json_file)
     return level_data["levels_constants"]
+
+
+def get_level_info(level, levels_data):
+    """Return level info"""
+    if levels_data and f"level{level}" in levels_data:
+        return levels_data[f"level{level}"]
