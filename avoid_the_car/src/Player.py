36716654298decfd1c11633590a74c0bf49979e6
paichiwo <lzerucha@gmail.com>
2023-10-05 21:18:21 +0100

    Refactor player to have attribute: speed

--- avoid_the_car/src/Player.py
+++ avoid_the_car/src/Player.py
@@ -1,4 +1,5 @@
 import pygame
+from avoid_the_car.src.utils import get_level_info
 
 
 class Player(pygame.sprite.Sprite):
@@ -11,10 +12,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.rotozoom(self.image, 0, 2)
         self.rect = self.image.get_rect(midbottom=(300, 700))
 
+        self.speed = 70
+
     def movement(self):
         """Move the player's car left or right based on user input."""
         keys = pygame.key.get_pressed()
-
         if keys[pygame.K_LEFT]:
             self.rect.x -= 3
         if keys[pygame.K_RIGHT]:
@@ -27,6 +29,12 @@ class Player(pygame.sprite.Sprite):
         if self.rect.x <= 160:
             self.rect.x += 10
 
-    def update(self):
+    def update_speed(self, level, levels_data):
+        """Update player's speed"""
+        level_info = get_level_info(level, levels_data)
+        self.speed = level_info["speed"]
+
+    def update(self, level, levels_data):
         self.movement()
         self.stay_on_road()
+        self.update_speed(level, levels_data)
