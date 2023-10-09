e8d77ba73fab856ec3ac83d8e75ed91a9152323b
paichiwo <lzerucha@gmail.com>
2023-10-09 19:41:55 +0100

    Add more player images for animations, implement basic left and right animations

--- pyshmup/src/player.py
+++ pyshmup/src/player.py
@@ -11,8 +11,30 @@ class Player(pygame.sprite.Sprite):
 
         self.ship_mid_a = pygame.image.load("assets/img/ship/ship_middle_a.png").convert_alpha()
         self.ship_mid_b = pygame.image.load("assets/img/ship/ship_middle_b.png").convert_alpha()
-        self.frames = [self.ship_mid_a, self.ship_mid_b]
-        self.anim_index = 0
+
+        self.ship_left_1_a = pygame.image.load("assets/img/ship/ship_left_1a.png").convert_alpha()
+        self.ship_left_1_b = pygame.image.load("assets/img/ship/ship_left_1b.png").convert_alpha()
+        self.ship_left_2_a = pygame.image.load("assets/img/ship/ship_left_2a.png").convert_alpha()
+        self.ship_left_2_b = pygame.image.load("assets/img/ship/ship_left_2b.png").convert_alpha()
+
+        self.ship_right_1_a = pygame.image.load("assets/img/ship/ship_right_1a.png").convert_alpha()
+        self.ship_right_1_b = pygame.image.load("assets/img/ship/ship_right_1b.png").convert_alpha()
+        self.ship_right_2_a = pygame.image.load("assets/img/ship/ship_right_2a.png").convert_alpha()
+        self.ship_right_2_b = pygame.image.load("assets/img/ship/ship_right_2b.png").convert_alpha()
+
+        self.frames_mid = [self.ship_mid_a, self.ship_mid_b]
+        self.frames_mid_anim_index = 0
+
+        self.frames_left_1 = [self.ship_left_1_a, self.ship_left_1_b]
+        self.frames_left_1_anim_index = 0
+        self.frames_left_2 = [self.ship_left_2_a, self.ship_left_2_b]
+        self.frames_left_2_anim_index = 0
+
+        self.frames_right_1 = [self.ship_right_1_a, self.ship_right_1_b]
+        self.frames_right_1_anim_index = 0
+        self.frames_right_2 = [self.ship_right_2_a, self.ship_right_2_b]
+        self.frames_right_2_anim_index = 0
+
         self.image = None
         self.rect = None
 
@@ -20,28 +42,46 @@ class Player(pygame.sprite.Sprite):
 
     def render(self):
         """Render player image"""
-        self.image = self.frames[self.anim_index]
+        self.image = self.frames_mid[self.frames_mid_anim_index]
         self.rect = self.image.get_rect(midbottom=(self.bg_img_width / 2, self.window_height - 10))
 
-    def animate(self):
-        """Rules for animating the player"""
-        self.anim_index += 0.8
-        if self.anim_index >= len(self.frames):
-            self.anim_index = 0
-        self.image = self.frames[int(self.anim_index)]
+    def animate_mid(self):
+        self.frames_mid_anim_index += 0.8
+        if self.frames_mid_anim_index >= len(self.frames_mid):
+            self.frames_mid_anim_index = 0
+        self.image = self.frames_mid[int(self.frames_mid_anim_index)]
+
+    def animate_left(self):
+        self.frames_left_1_anim_index += 0.8
+        if self.frames_left_1_anim_index >= len(self.frames_left_1):
+            self.frames_left_1_anim_index = 0
+        self.image = self.frames_left_1[int(self.frames_left_1_anim_index)]
+
+    def animate_right(self):
+        self.frames_right_1_anim_index += 0.8
+        if self.frames_right_1_anim_index >= len(self.frames_right_1):
+            self.frames_right_1_anim_index = 0
+        self.image = self.frames_right_1[int(self.frames_right_1_anim_index)]
 
     def movement(self):
         """Rules for moving the player"""
+
+        self.animate_mid()
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_UP]:
             self.rect.y -= 2
+
         if keys[pygame.K_DOWN]:
             self.rect.y += 2
+
         if keys[pygame.K_LEFT]:
             self.rect.x -= 2
+            self.animate_left()
+
         if keys[pygame.K_RIGHT]:
             self.rect.x += 2
+            self.animate_right()
 
     def update(self):
-        self.animate()
         self.movement()
