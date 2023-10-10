9377ecdf98c82f2697436c31920568e04353273e
paichiwo <lzerucha@gmail.com>
2023-10-10 21:49:09 +0100

    apply fumes as separate sprite

--- pyshmup/src/player.py
+++ pyshmup/src/player.py
@@ -12,6 +12,7 @@ class Player(pygame.sprite.Sprite):
         self.window_height = window_height
 
         self.ship_mid = pygame.image.load("assets/img/ship/middle.png").convert_alpha()
+
         self.ship_left_1 = pygame.image.load("assets/img/ship/left_1.png").convert_alpha()
         self.ship_left_2 = pygame.image.load("assets/img/ship/left_2.png").convert_alpha()
         self.left_frames = [self.ship_left_1, self.ship_left_2]
@@ -19,6 +20,58 @@ class Player(pygame.sprite.Sprite):
 
         self.ship_right_1 = pygame.image.load("assets/img/ship/right_1.png").convert_alpha()
         self.ship_right_2 = pygame.image.load("assets/img/ship/right_2.png").convert_alpha()
+        self.right_frames = [self.ship_right_1, self.ship_right_2]
+        self.right_index = 0
+
+        self.image = None
+        self.rect = None
+
+        self.render()
+
+    def render(self):
+        """Render player image"""
+        self.image = self.ship_mid
+        self.rect = self.image.get_rect(midbottom=(self.bg_img_width / 2, self.window_height - 10))
+
+    def animate_left(self):
+        self.left_index += 0.5
+        if self.left_index >= len(self.left_frames):
+            self.left_index = 1
+        self.image = self.left_frames[int(self.left_index)]
+
+    def animate_right(self):
+        self.right_index += 0.5
+        if self.right_index >= len(self.right_frames):
+            self.right_index = 1
+        self.image = self.right_frames[int(self.right_index)]
+
+    def movement(self):
+        """Rules for moving the player"""
+        self.image = self.ship_mid
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y -= 2
+
+        if keys[pygame.K_DOWN]:
+            self.rect.y += 2
+
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 2
+            self.animate_left()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 2
+            self.animate_right()
+
+    def update(self):
+        self.movement()
+
+
+class Fumes(pygame.sprite.Sprite):
+
+    def __init__(self, *args):
+        super().__init__(*args)
 
         self.fumes_1 = pygame.image.load("assets/img/ship/fumes_1.png")
         self.fumes_2 = pygame.image.load("assets/img/ship/fumes_2.png")
@@ -28,45 +81,19 @@ class Player(pygame.sprite.Sprite):
         self.image = None
         self.rect = None
 
-        self.fumes_image = None
-        self.fumes_rect = None
-
         self.render()
 
     def render(self):
         """Render player image"""
-        self.image = self.ship_mid
-        self.rect = self.image.get_rect(midbottom=(self.bg_img_width / 2, self.window_height - 10))
+        self.image = self.fumes_1
+        self.rect = self.image.get_rect()
 
-    def animate_fumes(self):
+    def animate_fumes(self, player_pos):
         self.fumes_index += 0.8
         if self.fumes_index >= len(self.fumes_frames):
             self.fumes_index = 0
         self.image = self.fumes_frames[int(self.fumes_index)]
+        self.rect.midbottom = player_pos
 
-    def animate_left_(self):
-        self.left_index += 0.01
-        if self.left_index >= len(self.left_frames):
-            self.left_index = 1
-        self.image = self.left_frames[int(self.left_index)]
-
-    def movement(self):
-        """Rules for moving the player"""
-
-        self.animate_mid()
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
-            self.rect.y -= 2
-
-        if keys[pygame.K_DOWN]:
-            self.rect.y += 2
-
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 2
-
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 2
-
-    def update(self):
-        self.movement()
+    def update(self, player_pos):
+        self.animate_fumes(player_pos)
