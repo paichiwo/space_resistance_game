c140059de2afe4ca00dd57e1903a4a4748dc556c
paichiwo <lzerucha@gmail.com>
2023-10-10 20:19:48 +0100

    Split fumes from the ship

--- pyshmup/src/player.py
+++ pyshmup/src/player.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 
 
@@ -9,59 +11,44 @@ class Player(pygame.sprite.Sprite):
         self.bg_img_width = bg_img_width
         self.window_height = window_height
 
-        self.ship_mid_a = pygame.image.load("assets/img/ship/ship_middle_a.png").convert_alpha()
-        self.ship_mid_b = pygame.image.load("assets/img/ship/ship_middle_b.png").convert_alpha()
+        self.ship_mid = pygame.image.load("assets/img/ship/middle.png").convert_alpha()
+        self.ship_left_1 = pygame.image.load("assets/img/ship/left_1.png").convert_alpha()
+        self.ship_left_2 = pygame.image.load("assets/img/ship/left_2.png").convert_alpha()
+        self.left_frames = [self.ship_left_1, self.ship_left_2]
+        self.left_index = 0
 
-        self.ship_left_1_a = pygame.image.load("assets/img/ship/ship_left_1a.png").convert_alpha()
-        self.ship_left_1_b = pygame.image.load("assets/img/ship/ship_left_1b.png").convert_alpha()
-        self.ship_left_2_a = pygame.image.load("assets/img/ship/ship_left_2a.png").convert_alpha()
-        self.ship_left_2_b = pygame.image.load("assets/img/ship/ship_left_2b.png").convert_alpha()
+        self.ship_right_1 = pygame.image.load("assets/img/ship/right_1.png").convert_alpha()
+        self.ship_right_2 = pygame.image.load("assets/img/ship/right_2.png").convert_alpha()
 
-        self.ship_right_1_a = pygame.image.load("assets/img/ship/ship_right_1a.png").convert_alpha()
-        self.ship_right_1_b = pygame.image.load("assets/img/ship/ship_right_1b.png").convert_alpha()
-        self.ship_right_2_a = pygame.image.load("assets/img/ship/ship_right_2a.png").convert_alpha()
-        self.ship_right_2_b = pygame.image.load("assets/img/ship/ship_right_2b.png").convert_alpha()
-
-        self.frames_mid = [self.ship_mid_a, self.ship_mid_b]
-        self.frames_mid_anim_index = 0
-
-        self.frames_left_1 = [self.ship_left_1_a, self.ship_left_1_b]
-        self.frames_left_1_anim_index = 0
-        self.frames_left_2 = [self.ship_left_2_a, self.ship_left_2_b]
-        self.frames_left_2_anim_index = 0
-
-        self.frames_right_1 = [self.ship_right_1_a, self.ship_right_1_b, self.ship_right_2_a]
-        self.frames_right_1_anim_index = 0
-        self.frames_right_2 = [self.ship_right_2_a, self.ship_right_2_b]
-        self.frames_right_2_anim_index = 0
+        self.fumes_1 = pygame.image.load("assets/img/ship/fumes_1.png")
+        self.fumes_2 = pygame.image.load("assets/img/ship/fumes_2.png")
+        self.fumes_frames = [self.fumes_1, self.fumes_2]
+        self.fumes_index = 0
 
         self.image = None
         self.rect = None
+
+        self.fumes_image = None
+        self.fumes_rect = None
 
         self.render()
 
     def render(self):
         """Render player image"""
-        self.image = self.frames_mid[self.frames_mid_anim_index]
+        self.image = self.ship_mid
         self.rect = self.image.get_rect(midbottom=(self.bg_img_width / 2, self.window_height - 10))
 
-    def animate_mid(self):
-        self.frames_mid_anim_index += 0.8
-        if self.frames_mid_anim_index >= len(self.frames_mid):
-            self.frames_mid_anim_index = 0
-        self.image = self.frames_mid[int(self.frames_mid_anim_index)]
+    def animate_fumes(self):
+        self.fumes_index += 0.8
+        if self.fumes_index >= len(self.fumes_frames):
+            self.fumes_index = 0
+        self.image = self.fumes_frames[int(self.fumes_index)]
 
-    def animate_left(self):
-        self.frames_left_1_anim_index += 0.8
-        if self.frames_left_1_anim_index >= len(self.frames_left_1):
-            self.frames_left_1_anim_index = 0
-        self.image = self.frames_left_1[int(self.frames_left_1_anim_index)]
-
-    def animate_right(self):
-        self.frames_right_1_anim_index += 0.8
-        if self.frames_right_1_anim_index >= len(self.frames_right_1):
-            self.frames_right_1_anim_index = 0
-        self.image = self.frames_right_1[int(self.frames_right_1_anim_index)]
+    def animate_left_(self):
+        self.left_index += 0.01
+        if self.left_index >= len(self.left_frames):
+            self.left_index = 1
+        self.image = self.left_frames[int(self.left_index)]
 
     def movement(self):
         """Rules for moving the player"""
@@ -77,13 +64,9 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_LEFT]:
             self.rect.x -= 2
-            self.animate_left()
-            # measure key pressed
-            # animate most left (left 2)
 
         if keys[pygame.K_RIGHT]:
             self.rect.x += 2
-            self.animate_right()
 
     def update(self):
         self.movement()
