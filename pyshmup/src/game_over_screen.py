233f3bd4e17be4397c757c4ec049cba6fb85af3d
paichiwo <lzerucha@gmail.com>
2023-10-20 13:34:49 +0100

    Change font and placement

--- pyshmup/src/game_over_screen.py
+++ pyshmup/src/game_over_screen.py
@@ -6,7 +6,7 @@ class GameOverScreen:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.screen = screen
-        self.font = pygame.font.Font(None, 30)
+        self.font = pygame.font.Font("assets/font/visitor1.ttf", 10)
 
     def show(self):
 
@@ -18,8 +18,8 @@ class GameOverScreen:
         game_over_rect = game_over_text.get_rect()
         restart_rect = restart_text.get_rect()
 
-        game_over_rect.center = (self.screen_width // 2, self.screen_height // 2 - 50)
-        restart_rect.center = (self.screen_width // 2, self.screen_height // 2 + 50)
+        game_over_rect.center = (self.screen_width // 2, self.screen_height // 2)
+        restart_rect.center = (self.screen_width // 2, self.screen_height // 2 + 10)
 
         self.screen.blit(game_over_text, game_over_rect)
         self.screen.blit(restart_text, restart_rect)
