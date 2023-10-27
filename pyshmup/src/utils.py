d9bbda50fd6dd3b0502963f95ac14b5a352062e4
paichiwo <lzerucha@gmail.com>
2023-10-27 17:37:26 +0100

    Create enemy_choices method which returns enemy selection for each level

--- pyshmup/src/utils.py
+++ pyshmup/src/utils.py
@@ -2,6 +2,7 @@ import json
 
 
 class Config:
+    """Returns values from data/config.json"""
     def __init__(self):
 
         with open("data/config.json", "r") as json_file:
@@ -9,3 +10,6 @@ class Config:
 
     def color(self):
         return self.data["colors"]
+
+    def enemy_choices(self):
+        return self.data["enemy_choices"]
