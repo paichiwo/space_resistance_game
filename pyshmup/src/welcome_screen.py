75f0946f05c93226f810cdebff1437fd06c1daff
paichiwo <lzerucha@gmail.com>
2023-10-29 14:58:41 +0000

    Redesign welcome screen

--- pyshmup/src/welcome_screen.py
+++ pyshmup/src/welcome_screen.py
@@ -1,51 +1,79 @@
+import os
+
 import pygame
 
 
 class WelcomeScreen:
-    def __init__(self, screen, screen_width, screen_height):
+    def __init__(self, screen, screen_width, screen_height, colors):
         self.screen = screen
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.colors = colors
+        self.mid_screen = (self.screen_width // 2, self.screen_height // 2)
         self.font = pygame.font.Font("assets/font/visitor1.ttf", 10)
 
-        self.anim_1 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_1.png").convert_alpha()
-        self.anim_2 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_2.png").convert_alpha()
-        self.anim_3 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_3.png").convert_alpha()
-        self.anim_4 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_4.png").convert_alpha()
-        self.anim_5 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_5.png").convert_alpha()
-        self.anim_6 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_6.png").convert_alpha()
-        self.anim_7 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_7.png").convert_alpha()
-        self.anim_8 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_8.png").convert_alpha()
-        self.anim_9 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_9.png").convert_alpha()
-        self.anim_10 = pygame.image.load("assets/img/ui/welcome_screen/screen_anim_10.png").convert_alpha()
-        self.screen_anim_frames = [self.anim_1, self.anim_2, self.anim_3, self.anim_4, self.anim_5,
-                                   self.anim_6, self.anim_7, self.anim_8, self.anim_9, self.anim_10]
-        self.screen_anim_index = 0
+        self.bg_dir = "assets/img/ui/welcome_screen"
+        self.bg_frames = []
+        self.bg_index = 0
+        for bg_image in os.listdir(self.bg_dir):
+            if bg_image.endswith(".png"):
+                bg_filename = os.path.join(self.bg_dir, bg_image)
+                bg_frame = pygame.image.load(bg_filename).convert_alpha()
+                self.bg_frames.append(bg_frame)
 
-        self.anim_delay = 100
+        self.planet_dir = "assets/img/ui/rotating_planet"
+        self.planet_frames = []
+        self.planet_index = 0
+        for planet_image in os.listdir(self.planet_dir):
+            if planet_image.endswith(".png"):
+                planet_filename = os.path.join(self.planet_dir, planet_image)
+                planet_frame = pygame.image.load(planet_filename).convert_alpha()
+                self.planet_frames.append(planet_frame)
+
+        self.planet_glow = pygame.image.load("assets/img/ui/rotating_planet/glow/glow.png")
+        self.logo = pygame.image.load("assets/img/ui/space_resistance_logo.png").convert_alpha()
+
+        self.anim_delay1 = 100
+        self.anim_delay2 = 100
         self.last_anim_update = pygame.time.get_ticks()
 
     def show(self):
 
-        cur = pygame.time.get_ticks()
-        if cur - self.last_anim_update >= self.anim_delay:
-            self.screen_anim_index = (self.screen_anim_index + 1) % len(self.screen_anim_frames)
-            self.last_anim_update = cur
-
         self.screen.fill("black")
 
-        anim_frame = self.screen_anim_frames[self.screen_anim_index]
-        anim_rect = anim_frame.get_rect(topleft=(0, 0))
-        self.screen.blit(anim_frame, anim_rect)
+        # Background animation
+        cur = pygame.time.get_ticks()
+        if cur - self.last_anim_update >= self.anim_delay1:
+            self.bg_index = (self.bg_index + 1) % len(self.bg_frames)
+            self.last_anim_update = cur
 
-        message_text = self.font.render("PYTHON SHOOT'EM UP GAME", True, "white")
-        start_text = self.font.render("Press 'S' to START", True, "white")
+        bg_frame = self.bg_frames[self.bg_index]
+        self.screen.blit(bg_frame, (0, 0))
 
-        message_rect = message_text.get_rect()
-        start_rect = start_text.get_rect()
+        # Planet animation
+        cur = pygame.time.get_ticks()
+        if cur - self.last_anim_update >= self.anim_delay2:
+            self.planet_index = (self.planet_index + 1) % len(self.planet_frames)
+            self.last_anim_update = cur
 
-        message_rect.center = (self.screen_width // 2, self.screen_height // 2)
-        start_rect.center = (self.screen_width // 2, self.screen_height // 2 + 10)
+        glow_rect = self.planet_glow.get_rect(center=(self.mid_screen[0], self.mid_screen[1] - 20))
+        self.screen.blit(self.planet_glow, glow_rect)
 
+        planet_frame = self.planet_frames[self.planet_index]
+        planet_rect = planet_frame.get_rect(center=(self.mid_screen[0], self.mid_screen[1] - 20))
+        self.screen.blit(planet_frame, planet_rect)
+
+
+        # Logo display
+        logo_rect = self.logo.get_rect(center=(self.mid_screen[0], self.mid_screen[1] - 20))
+        self.screen.blit(self.logo, logo_rect)
+
+        # Text 1
+        message_text = self.font.render("MADE BY PAICHIWO USING PYTHON / PYGAME", True, self.colors["WHITE"])
+        message_rect = message_text.get_rect(center=(self.mid_screen[0], self.mid_screen[1] + 60))
         self.screen.blit(message_text, message_rect)
+
+        # Text 2
+        start_text = self.font.render("PRESS 'S' TO START", True, self.colors["WHITE"])
+        start_rect = start_text.get_rect(center=(self.mid_screen[0], self.mid_screen[1] + 70))
         self.screen.blit(start_text, start_rect)
