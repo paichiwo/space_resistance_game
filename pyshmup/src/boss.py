83195adccb946ac27e05b9f0024ee054d15638db
paichiwo <lzerucha@gmail.com>
2023-11-01 19:44:32 +0000

    Create movement method for the Boss class

--- pyshmup/src/boss.py
+++ pyshmup/src/boss.py
@@ -1,4 +1,5 @@
 import pygame
+from math import radians, sin, cos, pi
 
 
 class Boss(pygame.sprite.Sprite):
@@ -13,8 +14,23 @@ class Boss(pygame.sprite.Sprite):
         self.boss_frames = [self.boss_img_1, self.boss_img_2]
         self.boss_index = 0
 
+        self.energy = 50
+        self.bump_power = 60
+        self.shot_score = 24
+        self.kill_score = 48
+        self.can_shoot = True
+
+        self.speed = 1
+        self.shot_power = 20
+
+        self.direction = 1
+        self.vert_speed = 1
+
+        self.shots = pygame.sprite.Group()
+        self.last_shot_time = pygame.time.get_ticks()
+
         self.image = self.boss_frames[self.boss_index]
-        self.rect = self.image.get_rect(center=(self.bg_img_width, 10))
+        self.rect = self.image.get_rect(center=(self.bg_img_width // 2, 10))
 
     def animate(self):
         self.boss_index += 0.5
@@ -22,5 +38,14 @@ class Boss(pygame.sprite.Sprite):
             self.boss_index = 0
         self.image = self.boss_frames[int(self.boss_index)]
 
+    def movement(self):
+        if self.rect.y >= self.window_height - 100:
+            self.direction = -1  # Move up when close to the bottom
+        elif self.rect.y <= 10:
+            self.direction = 1  # Move down when close to the top
+
+        self.rect.y += self.direction * self.vert_speed
+
     def update(self):
         self.animate()
+        self.movement()
