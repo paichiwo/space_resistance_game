9ef99fd669c30769cd0071ee05c091b32705140c
paichiwo <lzerucha@gmail.com>
2024-06-30 12:46:00 +0100

    Began work on v1.5

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,39 +1,26 @@
-import os
-import pygame
+from src.config import *
+from src.helpers import import_assets, import_image
 
 
 class Dashboard:
-    """All dashboard text and calculations"""
-    def __init__(self, screen, config_colors):
+    def __init__(self, screen, player):
         super().__init__()
 
         self.screen = screen
-        self.color = config_colors
+        self.player = player
 
-        self.planet_dir = "assets/img/ui/rotating_planet_small"
-        self.planet_frames = []
+        self.planet_frames = import_assets('assets/img/ui/rotating_planet_small')
         self.planet_index = 0
-        for planet_image in os.listdir(self.planet_dir):
-            if planet_image.endswith(".png"):
-                planet_filename = os.path.join(self.planet_dir, planet_image)
-                planet_frame = pygame.image.load(planet_filename).convert_alpha()
-                self.planet_frames.append(planet_frame)
+        self.life_img = import_image('assets/img/ui/life/0.png')
 
-        self.font = pygame.font.Font("assets/font/visitor1.ttf", 10)
-        self.life_img = pygame.image.load("assets/img/ui/ship_8x8.png").convert_alpha()
-
-        self.headers = ["SCORE", "LIVES", "ENERGY"]
         self.headers_x_pos = 288
         self.headers_y_pos = [50, 80, 115, 160]
 
-        self.score = 0
-        self.lives = 3
-
     def draw_dashboard_bg(self):
-        pygame.draw.rect(self.screen, self.color["GREY"], pygame.Rect(256, 0, 64, 180))
+        pygame.draw.rect(self.screen, COLORS['GREY'], pygame.Rect(256, 0, 64, 180))
 
-    def draw_logo(self):
-        self.planet_index += 0.1
+    def draw_logo(self, dt):
+        self.planet_index += 18 * dt
         if self.planet_index >= len(self.planet_frames):
             self.planet_index = 0
         image = self.planet_frames[int(self.planet_index)]
@@ -41,46 +28,47 @@ class Dashboard:
         self.screen.blit(image, rect)
 
     def draw_headers(self):
-        text_list = [self.font.render(header, False, self.color["WHITE"]) for header in self.headers]
+        headers = ['SCORE', 'LIVES', 'ENERGY']
+        text_list = [FONT10.render(header, False, COLORS['WHITE']) for header in headers]
 
         for i, text in enumerate(text_list):
             rect = text.get_rect(midtop=(self.headers_x_pos, self.headers_y_pos[i]))
             self.screen.blit(text, rect)
 
     def draw_score(self):
-        text = self.font.render("{:06}".format(self.score), False, self.color["WHITE"])
+        text = FONT10.render('{:06}'.format(self.player.score), False, COLORS['WHITE'])
         rect = text.get_rect(midtop=(self.headers_x_pos, self.headers_y_pos[0] + 10))
         self.screen.blit(text, rect)
 
-    def draw_lives(self, lives):
+    def draw_lives(self):
         life_x = self.headers_x_pos - 16
         life_y = self.headers_y_pos[1] + 12
 
-        for _ in range(0, lives-1):
+        for _ in range(0, self.player.lives-1):
             self.screen.blit(self.life_img, (life_x, life_y))
             life_x += 12
 
-    def draw_energy(self, current_energy, max_energy):
+    def draw_energy(self):
         energy_bar_length = 50
-        ratio = max_energy / energy_bar_length
-        bar_width = current_energy / ratio
+        ratio = self.player.max_energy / energy_bar_length
+        bar_width = self.player.current_energy / ratio
 
         energy_bar = pygame.Rect(self.headers_x_pos-25, self.headers_y_pos[2]+14, bar_width, 8)
         energy_bar_outline = pygame.Rect(self.headers_x_pos-26, self.headers_y_pos[2]+13, energy_bar_length+2, 10)
 
-        pygame.draw.rect(self.screen, self.color["RED"], energy_bar)
-        pygame.draw.rect(self.screen, self.color["YELLOW"], energy_bar_outline, 1)
+        pygame.draw.rect(self.screen, COLORS['RED'], energy_bar)
+        pygame.draw.rect(self.screen, COLORS['YELLOW'], energy_bar_outline, 1)
 
     def draw_levels(self, level):
-        text = self.font.render("LEVEL {}".format(level), False, self.color["WHITE"])
+        text = FONT10.render('LEVEL {}'.format(level+1), False, COLORS['WHITE'])
         rect = text.get_rect(midtop=(self.headers_x_pos, self.headers_y_pos[3]))
         self.screen.blit(text, rect)
 
-    def update(self, lives, energy, max_energy, level):
+    def update(self, player, level, dt):
         self.draw_dashboard_bg()
-        self.draw_logo()
+        self.draw_logo(dt)
         self.draw_headers()
         self.draw_score()
-        self.draw_lives(lives)
-        self.draw_energy(energy, max_energy)
+        self.draw_lives()
+        self.draw_energy()
         self.draw_levels(level)
--- src/player.py
+++ src/player.py
@@ -1,224 +1,222 @@
-import pygame
+from src.config import *
+from src.helpers import import_image, import_assets
+from src.timer import Timer
+from src.sprites import Shot, Explosion
+from src.in_game_msg import Message
 
 
 class Player(pygame.sprite.Sprite):
-    """Create the Player object"""
-    def __init__(self, bg_img_width, window_height, sound_manager, *args):
-        super().__init__(*args)
+    def __init__(self, screen, sound_manager, enemy_sprites, group):
+        super().__init__(group)
 
-        self.bg_img_width = bg_img_width
-        self.window_height = window_height
+        self.screen = screen
         self.sound_manager = sound_manager
+        self.enemy_sprites = enemy_sprites
+        self.group = group
 
-        self.ship_mid = pygame.image.load("assets/img/ship/middle.png").convert_alpha()
+        # Import assets
+        self.ship_empty_frames = import_image('assets/img/ship/empty.png')
+        self.ship_idle_frames = import_image('assets/img/ship/idle/0.png')
+        self.ship_left_frames = import_assets('assets/img/ship/left/')
+        self.ship_right_frames = import_assets('assets/img/ship/right/')
+        self.god_mode_idle_frames = [self.ship_idle_frames, self.ship_empty_frames, self.ship_idle_frames]
+        self.god_mode_left_frames = [self.ship_left_frames[0], self.ship_empty_frames, self.ship_left_frames[1]]
+        self.god_mode_right_frames = [self.ship_right_frames[0], self.ship_empty_frames, self.ship_right_frames[1]]
+        self.frame_index = 0
 
-        self.ship_left_1 = pygame.image.load("assets/img/ship/left_1.png").convert_alpha()
-        self.ship_left_2 = pygame.image.load("assets/img/ship/left_2.png").convert_alpha()
-        self.left_frames = [self.ship_left_1, self.ship_left_2]
-        self.left_index = 0
+        # Player image and rect
+        self.image = self.ship_idle_frames
+        self.rect = self.image.get_rect(midbottom=(BACKGROUND_WIDTH // 2, HEIGHT - 10))
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
 
-        self.ship_right_1 = pygame.image.load("assets/img/ship/right_1.png").convert_alpha()
-        self.ship_right_2 = pygame.image.load("assets/img/ship/right_2.png").convert_alpha()
-        self.right_frames = [self.ship_right_1, self.ship_right_2]
-        self.right_index = 0
+        # Fumes img and rect
+        self.fumes_frames = import_assets('assets/img/ship/fumes')
+        self.god_mode_fumes_frames = [self.fumes_frames[0], self.ship_empty_frames, self.fumes_frames[1]]
+        self.fumes_index = 0
+        self.fumes_image = self.fumes_frames[0]
+        self.fumes_rect = self.image.get_rect()
 
-        self.empty = pygame.image.load("assets/img/ship/empty.png")
-
-        self.god_mode_mid_frames = [self.ship_mid, self.empty, self.ship_mid]
-        self.god_mode_mid_index = 0
-
-        self.god_mode_left_frames = [self.ship_left_1, self.empty, self.ship_left_2]
-        self.god_mode_left_index = 0
-
-        self.god_mode_right_frames = [self.ship_right_1, self.empty, self.ship_right_2]
-        self.god_mode_right_index = 0
-
-        self.image = self.ship_mid
-        self.rect = self.image.get_rect(midbottom=(self.bg_img_width // 2, self.window_height - 10))
-
-        self.cur_energy = 100
+        # Data
+        self.status = 'idle'
+        self.speed = 120
+        self.current_energy = 100
         self.max_energy = 100
         self.lives = 4
+        self.score = 0
+        self.enemy_kill_count = 0
+        self.game_over = False
 
-        self.shots = pygame.sprite.Group()
-        self.shot_cooldown = 0
+        # Shots
+        self.shots_group = pygame.sprite.Group()
         self.shot_speed = 10
         self.shot_power = 10
-        self.is_shooting = False
 
-    def animate_left(self):
-        self.left_index += 0.5
-        if self.left_index >= len(self.left_frames):
-            self.left_index = 1
-        self.image = self.left_frames[int(self.left_index)]
+        # Timers
+        self.shot_timer = Timer(200)
+        self.bump_timer = Timer(500)
+        self.god_mode_timer = Timer(5000, self.reset_god_mode)
 
-    def animate_right(self):
-        self.right_index += 0.5
-        if self.right_index >= len(self.right_frames):
-            self.right_index = 1
-        self.image = self.right_frames[int(self.right_index)]
+        # Extra
+        self.god_mode = False
+        self.boss_killed = False
+        self.message = Message(self.screen, 'LIFE LOST', 3000)
 
-    def animate_god_mode_mid(self):
-        self.god_mode_mid_index += 0.5
-        if self.god_mode_mid_index >= len(self.god_mode_mid_frames):
-            self.god_mode_mid_index = 1
-        self.image = self.god_mode_mid_frames[int(self.god_mode_mid_index)]
-
-    def animate_god_mode_left(self):
-        self.god_mode_left_index += 0.5
-        if self.god_mode_left_index >= len(self.god_mode_left_frames):
-            self.god_mode_left_index = 1
-        self.image = self.god_mode_left_frames[int(self.god_mode_left_index)]
-
-    def animate_god_mode_right(self):
-        self.god_mode_right_index += 0.5
-        if self.god_mode_right_index >= len(self.god_mode_right_frames):
-            self.god_mode_right_index = 1
-        self.image = self.god_mode_right_frames[int(self.god_mode_right_index)]
-
-    def movement_animation_normal_mode(self, keys):
-        self.image = self.ship_mid
-        if keys[pygame.K_UP]:
-            self.rect.y -= 2
-        if keys[pygame.K_DOWN]:
-            self.rect.y += 2
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 2
-            self.animate_left()
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 2
-            self.animate_right()
-
-    def movement_animation_god_mode(self, keys):
-        self.animate_god_mode_mid()
-        if keys[pygame.K_UP]:
-            self.rect.y -= 2
-        if keys[pygame.K_DOWN]:
-            self.rect.y += 2
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 2
-            self.animate_god_mode_left()
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 2
-            self.animate_god_mode_right()
-
-    def movement(self, god_mode):
-        """Rules for moving the player"""
-        keys = pygame.key.get_pressed()
-        if not god_mode:
-            self.movement_animation_normal_mode(keys)
+    def animate_player(self, frames, dt):
+        if not self.god_mode:
+            self.frame_index += 1 * dt
         else:
-            self.movement_animation_god_mode(keys)
+            self.frame_index += 10 * dt
 
-    def movement_after_boss_killed(self):
-        self.image = self.ship_mid
-        self.rect.y -= 2
+        if self.frame_index >= len(frames):
+            self.frame_index = 1
+        self.image = frames[int(self.frame_index)]
 
-    def action(self):
+    def animate_fumes(self, dt):
+        if not self.god_mode:
+            self.fumes_index += 20 * dt
+            if self.fumes_index >= len(self.fumes_frames):
+                self.fumes_index = 0
+            self.fumes_image = self.fumes_frames[int(self.fumes_index)]
+        else:
+            self.fumes_image = self.ship_empty_frames
+
+    def animate(self, dt):
+        frames = {
+            'idle': self.god_mode_idle_frames if self.god_mode else self.ship_idle_frames,
+            'left': self.god_mode_left_frames if self.god_mode else self.ship_left_frames,
+            'right': self.god_mode_right_frames if self.god_mode else self.ship_right_frames,
+        }
+
+        if self.status == 'idle' and not self.god_mode:
+            self.image = frames['idle']
+        else:
+            self.animate_player(frames[self.status], dt)
+
+        self.animate_fumes(dt)
+
+    def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and not self.is_shooting:
-            self.shoot()
-            self.is_shooting = True
-            self.sound_manager.play_player_shot_fx()
 
-        elif not keys[pygame.K_SPACE]:
-            self.is_shooting = False
+        self.direction.y = -1 if keys[pygame.K_UP] else (1 if keys[pygame.K_DOWN] else 0)
+        self.direction.x = -1 if keys[pygame.K_LEFT] else (1 if keys[pygame.K_RIGHT] else 0)
+
+        if keys[pygame.K_LEFT]:
+            self.status = 'left'
+        elif keys[pygame.K_RIGHT]:
+            self.status = 'right'
+        else:
+            self.status = 'idle'
+
+        if keys[pygame.K_SPACE] and not self.shot_timer.active:
+            Shot(self.rect, self.shots_group, 'player')
+            self.sound_manager.play_sound(SOUND_EFFECTS['player_shot'])
+            self.shot_timer.activate()
+
+    def move_fumes(self):
+        self.screen.blit(self.fumes_image, self.fumes_rect)
+        self.fumes_rect.midtop = self.rect.midbottom[0], self.rect.midbottom[1] - 3
+
+    def move(self, dt):
+        self.move_fumes()
+
+        if self.direction.magnitude() > 0:
+            self.direction = round(self.direction.normalize())
+
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = (round(self.pos.x), round(self.pos.y))
+
+    def move_boss_killed(self):
+        self.image = self.ship_idle_frames
+        self.pos.y -= 2
+        self.rect.topleft = self.pos
 
     def stay_within_boundaries(self):
-        """Rules to stay on the screen"""
         if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > self.bg_img_width:
-            self.rect.right = self.bg_img_width
+            self.pos.x = self.rect.width / 2
+        elif self.rect.right > BACKGROUND_WIDTH:
+            self.pos.x = BACKGROUND_WIDTH - self.rect.width / 2
+
         if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.bottom > self.window_height:
-            self.rect.bottom = self.window_height
+            self.pos.y = self.rect.height / 2
+        elif self.rect.bottom > HEIGHT:
+            self.pos.y = HEIGHT - self.rect.height / 2
 
-    def shoot(self):
-        if self.shot_cooldown == 0:
-            new_shot = Shot(self.rect)
-            self.shots.add(new_shot)
-            self.shot_cooldown = self.shot_speed
+        self.rect.center = self.pos
 
-    def handle_shot_cooldown(self):
-        if self.shot_cooldown > 0:
-            self.shot_cooldown -= 1
+    def check_collisions(self):
+        # shots
+        for shot in self.shots_group:
+            hits = pygame.sprite.spritecollide(shot, self.enemy_sprites, False)
+            if hits:
+                shot.kill()
+                for enemy in hits:
+                    if enemy.energy > 0:
+                        enemy.deduct_energy(self.shot_power)
+                        self.score += enemy.shot_score
+                    if enemy.energy <= 0:
+                        self.enemy_kill_count += 1
+                        self.score += enemy.kill_score
+
+                    Explosion(enemy.rect.center, self.group)
+                    self.sound_manager.play_sound(SOUND_EFFECTS['explosion'])
+        # bumps
+        if not self.bump_timer.active:
+            bumps = pygame.sprite.spritecollide(self, self.enemy_sprites, False)
+            for enemy in bumps:
+                if not self.god_mode:
+                    self.get_damage(enemy.bump_power)
+                    Explosion(self.rect.center, self.group)
+                    self.sound_manager.play_sound(SOUND_EFFECTS['explosion'])
+                    self.bump_timer.activate()
 
     def get_damage(self, damage_value):
-        if self.cur_energy > 0:
-            self.cur_energy -= damage_value
+        if not self.god_mode and self.current_energy > 0:
+            self.current_energy -= damage_value
 
-    def update(self, god_mode, boss_killed):
-        if not boss_killed:
-            self.movement(god_mode)
-            self.action()
-            self.stay_within_boundaries()
-            self.shots.update()
-            self.handle_shot_cooldown()
-        else:
-            self.movement_after_boss_killed()
+    def deduct_life(self):
+        if self.current_energy <= 0:
+            self.current_energy = 0
+            self.message.show()
+            self.sound_manager.play_sound(SOUND_EFFECTS['lost_life'])
+            self.lives -= 1
+            self.current_energy = self.max_energy
+            self.god_mode = True
+            self.god_mode_timer.activate()
+            if self.lives <= 0:
+                self.game_over = True
 
+    def reset_god_mode(self):
+        self.god_mode = False
 
-class Fumes(pygame.sprite.Sprite):
+    def reset(self):
+        self.score = 0
+        self.enemy_kill_count = 0
+        self.lives = 4
+        self.current_energy = 100
+        self.rect = self.image.get_rect(midbottom=(BACKGROUND_WIDTH // 2, HEIGHT - 10))
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.direction = pygame.math.Vector2()
+        self.god_mode = False
+        self.boss_killed = False
+        self.game_over = False
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def update_groups(self, dt):
+        self.shots_group.update(dt)
+        self.shots_group.draw(self.screen)
 
-        self.fumes_1 = pygame.image.load("assets/img/ship/fumes_1.png").convert_alpha()
-        self.fumes_2 = pygame.image.load("assets/img/ship/fumes_2.png").convert_alpha()
-        self.fumes_frames = [self.fumes_1, self.fumes_2]
-        self.fumes_index = 0
+    def update_timers(self):
+        self.shot_timer.update()
+        self.bump_timer.update()
+        self.god_mode_timer.update()
 
-        self.image = None
-        self.rect = None
-
-        self.render()
-
-    def render(self):
-        """Render player image"""
-        self.image = self.fumes_1
-        self.rect = self.image.get_rect()
-
-    def animate_fumes(self, player_pos):
-        self.fumes_index += 0.5
-        if self.fumes_index >= len(self.fumes_frames):
-            self.fumes_index = 0
-        self.image = self.fumes_frames[int(self.fumes_index)]
-        self.rect.midbottom = player_pos
-
-    def update(self, player_pos):
-        self.animate_fumes(player_pos)
-
-
-class Shot(pygame.sprite.Sprite):
-
-    def __init__(self, player_rect):
-        super().__init__()
-
-        self.laser_1 = pygame.image.load("assets/img/shot/laser_a.png").convert_alpha()
-        self.laser_2 = pygame.image.load("assets/img/shot/laser_b.png").convert_alpha()
-        self.laser_frames = [self.laser_1, self.laser_2]
-        self.laser_index = 0
-
-        self.image = self.laser_frames[self.laser_index]
-        self.rect = self.image.get_rect(midbottom=player_rect.midtop)
-        self.rect.x -= 1
-
-    def animate(self):
-        self.laser_index += 0.5
-        if self.laser_index >= len(self.laser_frames):
-            self.laser_index = 0
-        self.image = self.laser_frames[int(self.laser_index)]
-
-    def movement(self):
-        self.rect.y -= 5
-
-    def kill_off_screen(self):
-        if self.rect.bottom < 0:
-            self.kill()
-
-    def update(self):
-        self.animate()
-        self.movement()
-        self.kill_off_screen()
+    def update(self, dt):
+        self.update_timers()
+        self.input()
+        self.move(dt)
+        self.animate(dt)
+        self.stay_within_boundaries()
+        self.update_groups(dt)
+        self.check_collisions()
+        self.deduct_life()
+        self.message.update()
