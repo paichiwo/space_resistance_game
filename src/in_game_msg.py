33f552e78117004327d5c5601353e7a21f4aa639
paichiwo <lzerucha@gmail.com>
2024-07-09 21:48:21 +0100

    Create class for a message between levels

--- src/in_game_msg.py
+++ src/in_game_msg.py
@@ -24,3 +24,16 @@ class Message:
         if self.visible:
             self.screen.blit(self.outline, (BACKGROUND_WIDTH // 2 - 54, HEIGHT // 2 - 19))
             self.screen.blit(self.text, (BACKGROUND_WIDTH // 2 - 55, HEIGHT // 2 - 20))
+
+
+class MessageBetweenLevels:
+    def __init__(self, screen, text_array):
+        self.screen = screen
+
+        self.texts = [(FONT20.render(text_array[0], False, COLORS['WHITE']), (BACKGROUND_WIDTH // 2, HEIGHT // 2)),
+                      (FONT10.render(text_array[1], False, COLORS['WHITE']), (BACKGROUND_WIDTH // 2, HEIGHT // 2 + 30))]
+
+    def show(self):
+        for text, pos in self.texts:
+            self.screen.blit(text, text.get_rect(midtop=pos))
+
