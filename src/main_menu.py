0f87395bd40939f140f1893049ab847248a1201f
paichiwo <lzerucha@gmail.com>
2024-07-29 19:53:46 +0100

    main menu fully implemented

--- src/main_menu.py
+++ src/main_menu.py
@@ -8,27 +8,29 @@ class MainMenu:
         self.states = states
         self.sound_manager = sound_manager
         self.restart_game = restart_game
-
         self.scale = SCALE
-        self.volume_level = 0.5
-        self.options_selected = False
-        self.fullscreen = False
-
-        self.menu_items = {
-            'main': ['start game', 'options'],
-            'options': ['scale', 'fullscreen', f'volume: {self.volume_level}', 'accept']
-        }
-
-        self.rects = {
-            'main': {item: pygame.rect.Rect() for item in self.menu_items['main']},
-            'options': {item: pygame.rect.Rect() for item in self.menu_items['options']}
-        }
 
         self.selected_index = 0
         self.navigate_delay = 200
         self.button_delay = 200
         self.last_navigate_time = pygame.time.get_ticks()
         self.last_button_time = pygame.time.get_ticks()
+        self.button_interaction_state = None
+        self.button_interaction_time = 0
+        self.volume_level = 50
+        self.options_selected = False
+        self.fullscreen = False
+
+        self.menu_items = {
+            'main': ['start game', 'options'],
+            'options': ['scale', 'fullscreen', f'volume: {self.volume_level}', 'accept'],
+            'volume': ['main', 'inside', 'minus', 'plus']
+        }
+        self.rects = {
+            'main': {item: pygame.rect.Rect() for item in self.menu_items['main']},
+            'options': {item: pygame.rect.Rect() for item in self.menu_items['options']},
+            'volume': {item: pygame.rect.Rect() for item in self.menu_items['volume']}
+        }
 
     def draw_menu(self, items):
         if self.options_selected:
@@ -37,7 +39,10 @@ class MainMenu:
         x = WIDTH // 2
         y = 180
         for index, item in enumerate(items):
-            item_color = COLORS['GREEN'] if index == self.selected_index else COLORS['WHITE']
+            if item == f'volume: {self.volume_level}':
+                item_color = COLORS['WHITE']
+            else:
+                item_color = COLORS['INDIGO'] if index == self.selected_index else COLORS['WHITE']
             text = FONT10.render(item, True, item_color)
             rect = text.get_rect(center=(x, y))
             self.rects['main' if not self.options_selected else 'options'][item] = rect
@@ -45,28 +50,69 @@ class MainMenu:
             y += 10
 
     def draw_volume_bar(self):
-        max_width = 102
+        width = 102
         height = 11
-        border_color = COLORS['WHITE']
-        fill_color = COLORS['GREEN']
 
-        bg_rect = pygame.rect.Rect(WIDTH // 2 - max_width // 2, HEIGHT // 2 + 69, max_width, height)
-        pygame.draw.rect(self.screen, border_color, bg_rect, 1)
+        selected = (self.selected_index == self.menu_items['options'].index(f'volume: {self.volume_level}'))
 
-        fg_rect = pygame.rect.Rect(bg_rect.x + 2, bg_rect.y + 2, (max_width - 4) * self.volume_level / 100, height - 4)
-        pygame.draw.rect(self.screen, fill_color, fg_rect)
+        # bar
+        self.rects['volume']['main'] = pygame.rect.Rect(WIDTH // 2 - width // 2,
+                                                        HEIGHT // 2 + 69,
+                                                        width,
+                                                        height)
+        self.rects['volume']['inside'] = pygame.rect.Rect(self.rects['volume']['main'].x + 2,
+                                                          self.rects['volume']['main'].y + 2,
+                                                          (width - 4) * self.volume_level / 100,
+                                                          height - 4)
+        # draw bar
+        pygame.draw.rect(self.screen, COLORS['INDIGO'] if selected else COLORS['WHITE'],
+                         self.rects['volume']['main'], 1)
+        pygame.draw.rect(self.screen, COLORS['INDIGO'], self.rects['volume']['inside'])
 
-        self.menu_items['options'][2] = f'volume: {self.volume_level}'
+        # buttons
+        self.rects['volume']['minus'] = pygame.rect.Rect(self.rects['volume']['main'].x - height - 1,
+                                                         self.rects['volume']['main'].y,
+                                                         height,
+                                                         height)
+        self.rects['volume']['plus'] = pygame.rect.Rect(self.rects['volume']['main'].x + width + 1,
+                                                        self.rects['volume']['main'].y,
+                                                        height,
+                                                        height)
+
+        # draw button effect
+        minus_color = self.get_button_color('minus')
+        plus_color = self.get_button_color('plus')
+        pygame.draw.rect(self.screen, minus_color, self.rects['volume']['minus'])
+        pygame.draw.rect(self.screen, plus_color, self.rects['volume']['plus'])
+
+        # draw buttons
+        pygame.draw.rect(self.screen, COLORS['WHITE'], self.rects['volume']['minus'], 1)
+        pygame.draw.rect(self.screen, COLORS['WHITE'], self.rects['volume']['plus'], 1)
+
+        minus_text = FONT10.render('-', True, COLORS['WHITE'])
+        plus_text = FONT10.render('+', True, COLORS['WHITE'])
+
+        self.screen.blit(minus_text, minus_text.get_rect(center=(self.rects['volume']['main'].x - 6,
+                                                                 self.rects['volume']['main'].y + 6)))
+        self.screen.blit(plus_text, plus_text.get_rect(center=(self.rects['volume']['main'].x + width + 7,
+                                                               self.rects['volume']['main'].y + 6)))
 
     def input(self, event):
         current_time = pygame.time.get_ticks()
 
         if event.type == pygame.JOYHATMOTION:
             if current_time - self.last_navigate_time > self.navigate_delay:
-                if event.value[1] == 1:  # Up
+                if event.value[1] == 1:
                     self.selected_index = (self.selected_index - 1) % len(self.current_items())
-                elif event.value[1] == -1:  # Down
+                elif event.value[1] == -1:
                     self.selected_index = (self.selected_index + 1) % len(self.current_items())
+                elif event.value[0] == -1 and 'volume: ' in self.menu_items['options'][self.selected_index]:
+                    self.adjust_volume(-1)
+                    self.start_button_effect('minus')
+                elif event.value[0] == 1 and 'volume: ' in self.menu_items['options'][self.selected_index]:
+                    self.adjust_volume(1)
+                    self.start_button_effect('plus')
+
                 self.last_navigate_time = current_time
 
         if event.type == pygame.JOYAXISMOTION:
@@ -77,6 +123,15 @@ class MainMenu:
                             self.selected_index = (self.selected_index + 1) % len(self.current_items())
                         else:
                             self.selected_index = (self.selected_index - 1) % len(self.current_items())
+                        self.last_navigate_time = current_time
+                    elif event.axis == 0:
+                        if 'volume: ' in self.menu_items['options'][self.selected_index]:
+                            if event.value < 0:
+                                self.adjust_volume(-1)
+                                self.start_button_effect('minus')
+                            else:
+                                self.adjust_volume(1)
+                                self.start_button_effect('plus')
                         self.last_navigate_time = current_time
 
         if event.type == pygame.JOYBUTTONDOWN:
@@ -94,8 +149,10 @@ class MainMenu:
                 if f'volume: {self.volume_level}' in self.current_items():
                     if event.key == pygame.K_LEFT:
                         self.adjust_volume(-1)
+                        self.start_button_effect('minus')
                     elif event.key == pygame.K_RIGHT:
-                        self.adjust_volume(+1)
+                        self.adjust_volume(1)
+                        self.start_button_effect('plus')
                 self.last_navigate_time = current_time
 
             elif current_time - self.last_button_time > self.button_delay:
@@ -103,23 +160,32 @@ class MainMenu:
                     self.action(list(self.current_items().keys())[self.selected_index])
                 self.last_button_time = current_time
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for item, rect in self.current_items().items():
-                if rect and rect.collidepoint(event.pos):
-                    self.action(item)
-
         if event.type == pygame.MOUSEMOTION:
             for index, (item, rect) in enumerate(self.current_items().items()):
                 if rect.collidepoint(event.pos):
                     self.selected_index = index
                     break
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for item, rect in self.current_items().items():
+                if rect and rect.collidepoint(event.pos):
+                    self.action(item)
+
+            if self.options_selected:
+                if current_time - self.last_button_time > self.button_delay:
+                    if self.rects['volume']['minus'].collidepoint(event.pos):
+                        self.adjust_volume(-1)
+                        self.start_button_effect('minus')
+                    elif self.rects['volume']['plus'].collidepoint(event.pos):
+                        self.adjust_volume(1)
+                        self.start_button_effect('plus')
+                    self.last_button_time = current_time
+
     def adjust_volume(self, change):
         self.volume_level = max(0, min(100, self.volume_level + change))
-        self.sound_manager.set_volume(self.volume_level / 100)
+        self.sound_manager.set_master_volume(self.volume_level / 100)
+        self.menu_items['options'][2] = f'volume: {self.volume_level}'
 
-    def handle_volume_level(self):
-        pass
 
     def action(self, item):
         if item == 'start game':
@@ -130,17 +196,16 @@ class MainMenu:
         elif item == 'options':
             self.options_selected = True
             self.selected_index = 0
+
         elif item == 'scale':
             self.scale += 1
             if self.scale >= 5:
                 self.scale = 1
             self.window.size = (WIDTH * self.scale, HEIGHT * self.scale)
+
         elif item == 'fullscreen':
             self.fullscreen = not self.fullscreen
             self.window.set_fullscreen(True) if self.fullscreen else self.window.set_windowed()
-
-        elif item == f'volume: {self.volume_level}':
-            self.handle_volume_level()
 
         elif item == 'accept':
             self.options_selected = False
@@ -148,6 +213,31 @@ class MainMenu:
 
     def current_items(self):
         return self.rects['options'] if self.options_selected else self.rects['main']
+
+    def get_button_color(self, button_type):
+        base_color = COLORS['BLACK']
+        target_color = COLORS['INDIGO']
+        duration = 100
+
+        if self.button_interaction_state == button_type:
+            elapsed_time = pygame.time.get_ticks() - self.button_interaction_time
+            if elapsed_time > duration:
+                self.button_interaction_state = None
+                return base_color
+
+            t = (elapsed_time / duration)
+
+            return (
+                int(base_color[0] * (1 - t) + target_color[0] * t),
+                int(base_color[1] * (1 - t) + target_color[1] * t),
+                int(base_color[2] * (1 - t) + target_color[2] * t),
+            )
+
+        return base_color
+
+    def start_button_effect(self, button_type):
+        self.button_interaction_state = button_type
+        self.button_interaction_time = pygame.time.get_ticks()
 
     def update(self, event):
         self.input(event)
